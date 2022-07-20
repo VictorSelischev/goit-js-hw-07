6d8cf9be9e60b54d82423d1478c5777e4884b6ad
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-07-20 22:17:36 +0300

    Решает задачу 1 модуля 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,60 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+gallery.innerHTML = createGalleryLi(galleryItems);
+
+gallery.addEventListener('click', handleGalleryItemClick);
+
+
+
+
+
+// FUNCTION ==================================
+
+function createGalleryLi(array) {
+    return array
+    .map(item => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+            </a>
+        </div>`
+    })
+    .join('');
+}
+
+function handleGalleryItemClick(event) {
+    event.preventDefault();
+    
+    const galleryItem = event.target;
+    // console.log(galleryItem);
+    const urlImgBig = galleryItem.dataset.source;
+    // console.log(urlImgBig);
+
+    openModalGallery(urlImgBig);
+}
+
+function openModalGallery(url) {
+    const modalItem = basicLightbox.create(`
+    <img src="${url}" width="800" height="600">
+`);
+    return modalItem.show();
+}
+
+
+
+
+
+
+
+
+
