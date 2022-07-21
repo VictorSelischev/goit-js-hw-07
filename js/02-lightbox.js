1e174817180e96bedf4dc1a3d646e558f19040f5
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-07-21 18:53:06 +0300

    Решает задачу 2 модуля 7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
+// import {SimpleLightbox} from "simplelightbox";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+gallery.innerHTML = createGalleryLi(galleryItems);
+
+const simplelightbox = new SimpleLightbox(".gallery a", {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+
+
+// gallery.addEventListener('click', handleGalleryItemClick);
+
+
+
+// FUNCTION ==================================
+
+function createGalleryLi(array) {
+    return array
+    .map(item => {
+        return `
+        <a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>`
+    })
+    .join('');
+}
+
+// function handleGalleryItemClick(event) {
+//     event.preventDefault();
+//     const galleryLink = new SimpleLightbox(".gallery a", {
+//             captionsData: "alt",
+//             captionDelay: 250,
+//         });
+
+
+// }
+
